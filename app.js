6e5a5fc522cd19525ee7f8e7a9c195e9e4c4f250
Matt Steiner <Metropolis.4488@gmail.com>
2020-10-11 16:34:05 -0400

    minor change

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+roster();
+async function roster() {
+    const {} = await inquirer.prompt([{
+        
+    }])
+}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
